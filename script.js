a8ad12120364c50b2f8107e00ac4b2213665ada2
kulikovavarvara <151393496+kulikovavarvara@users.noreply.github.com>
2024-10-13 13:39:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,135 @@
+/*let a = 6
+if (a > 4) {
+    console.log(true)
+}
+else{
+    console.log(false)
+}
+
+function getData(fun) {
+    console.log(true);
+    console.log(a);
+    console.log(b);
+}*/
+
+const numbers = [1, 3, 2, 5, 7, 9];
+
+//for
+
+/*for(let i = 0; i < numbers.length; 1++) {
+    if(numbers[i] % 2 == 0){
+        console.log(i);
+}
+    }
+    */
+
+    /*for(el of numbers) {
+        if (el % 2 ! = 0)
+        console.log(el)
+    }*/
+
+    //forEach
+
+   /* const fruits = ["яблоко", "груша", "апельсин", "банан" ];
+
+    fruits.forEach(el => console.log(el)) */
+
+    /*let arrNum = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let newArr = [];
+
+    for(el of arrNum) {
+        if(el > 5) {
+            newArr.push(el)
+        }
+    }
+    console.log(newArr)
+
+    let newArr = arrNum.filter(el => el > 5);
+    console.log(newArr);*/
+
+
+let arrNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+/*let newArr = [];
+
+for(let i = 0; i < arrNum.length; i++){
+    newArr.push(arrNum[i].toString)
+}*/
+
+/*let newArr = arrNum.map (() => {else.toString()})
+console.log(newArr)*/  
+
+/*let arrNum = [1, 45, 7, 8, 9];
+let sum = 0;
+
+for(let i = 0; i < arrNum.length; i ++) {
+    sum +- arrNum[i]
+}
+
+console.log(sum)
+
+let sum = arrNuM.reduce((sumCount, el) => sumCount + el, 0)
+console.log("сумма элементов ="+ sum)*/
+
+/*//задача 1
+let arrNuM= [1, 2, -3, 4, 56, -7, 89, -36, 32, -57];
+
+let arrPol = arrNum. filter(el => el > 0).length;
+console.log("кол-во + чисел =" + arrPol);
+
+let arrPol = arrNum. filter(el => el > 0).length;
+console.log("кол-во - чисел =" + arrPol);
+
+let strArr = ["hello", "str", "day"]; 
+let srrArrChange = strArr.map(el => el.toUpperCase());
+console.log(srrArrChange);
+
+const sentCount = sentences.map(el => el.split("").length)
+console.log(sentCount);
+
+//Практическая работа
+
+//задача 1
+function squareNum(arr){
+    return arr.map(el => el**2);
+}
+
+console.log(squareNum([1, 2, 5, 2, 5]));
+
+//задача 2
+let newArr = arrNum.filter(a => a % 5 == 0);
+    console.log(newArr);
+
+//задача 3
+
+const words = [ "apple", "banana", "cat", "dog"];
+const longWords = words.filter( word.length > 3);
+console.log(longWords);
+
+//задача 4
+
+function arrNum {
+    let sum = 0;
+    for (let i = 0: <arr.length; i++) {
+        
+        sum += arr[i];
+    }
+    console.log(sum);
+}
+
+//задача 6
+ 
+const arr = ['apple','banana','orange'];
+ const newArr = [];
+function strings {
+  arr.forEach(element => {
+  newArr.push(element.toUpperCase());
+});
+console.log(newArr);
+}
+
+//задача 7
+
+const numbers = [5, 12, 8, 130, 44];
+const filteredNumbers = numbers.filter(number => number > 10);
+console.log(filteredNumbers);  */
